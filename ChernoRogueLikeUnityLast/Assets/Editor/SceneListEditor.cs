66e16a7abfd211291c26f756b81d3431f5cf201f
SoraE1224 <sora-e1224@nifty.com>
2018-10-18 18:42:26 +0900

    再度

---
+++ ChernoRogueLikeUnityLast/Assets/Editor/SceneListEditor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(SceneManager))]
+public class SceneListEditor : Editor
+{
+    SceneManager sceneManager = null;
+
+    bool _isOpen = false;
+
+    void OnEnable()
+    {
+        sceneManager = (SceneManager)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+
+        bool isOpen = EditorGUILayout.Foldout(_isOpen, "SceneList");
+        if (_isOpen != isOpen)
+        {
+            _isOpen = isOpen;
+        }
+
+
+        if (isOpen)
+        {
+            for (int i = 0; i < sceneManager.sceneNameList.Count; )
+            {
+                var oldScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(sceneManager.sceneNameList[i]);
+                EditorGUILayout.BeginHorizontal();
+                var newScene = EditorGUILayout.ObjectField("Scene", oldScene, typeof(SceneAsset), false) as SceneAsset;
+                var newPath = AssetDatabase.GetAssetPath(newScene);
+                sceneManager.sceneNameList[i] = newPath;
+                if(GUILayout.Button("-",GUILayout.Width(32)))
+                {
+                    sceneManager.sceneNameList.RemoveAt(i);
+                    continue;
+                }
+                EditorGUILayout.EndHorizontal();
+                i++;
+            }
+
+            EditorGUILayout.LabelField("");
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("+", GUILayout.Width(32)))
+            {
+                sceneManager.sceneNameList.Add(null);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+}
