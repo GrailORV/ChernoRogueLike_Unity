b3a1df487145ca4dbefdd24992b3e64132ee3eeb
geskysan <sakuya3983@ezweb.ne.jp>
2018-10-25 19:30:09 +0900

    死の淵から蘇ったピエロの怒り （タイトル途中、チームロゴひとまず完成）

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/TeamLogo/unty.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class unty : MonoBehaviour {
+
+    [SerializeField] GameObject Logo;
+    [SerializeField] float fadeTime, IntercalTime;
+
+	// Use this for initialization
+	void Start () {
+        LogoFade();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LogoFade()
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(Logo.GetComponent<CanvasGroup>().DOFade(1f, fadeTime))
+            .AppendInterval(IntercalTime)
+            .Append(Logo.GetComponent<CanvasGroup>().DOFade(0f, fadeTime))
+            .OnComplete(() =>
+            {
+                Fade.FadeStart(Fade.FadeMode.FM_FadeOut, 1f, SceneChange);
+            });
+    }
+
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
