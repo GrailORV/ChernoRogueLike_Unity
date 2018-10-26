d8730d210e4e4edb2fe9f6020deae94c341fc151
SoraE1224 <sora-e1224@nifty.com>
2018-10-26 12:19:01 +0900

    Launch Creating MapManager

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/MapManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    enum MapCategory
+    {
+        MC_Grass,
+        MC_Cave
+    }
+
+    [SerializeField]
+    private MapCategory mCat;
+
+    [SerializeField]
+    private GameObject WallPrefab;
+
+    void Start()
+    {
+        if(!WallPrefab)
+        {
+            return;
+        }
+
+        for (int x = 0; x < 300; x++)
+        {
+            for (int z = 0; z < 300; z++)
+            {
+                Instantiate(WallPrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
