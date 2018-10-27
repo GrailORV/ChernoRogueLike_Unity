60bf43f03a6362985af21540589c479c7e8a71e5
ueno <saku39sky@gmail.com>
2018-10-28 01:49:55 +0900

    小川の〇玉の表示・非表示

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Scenes/Item/ItemUIControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUIControl : MonoBehaviour
+{
+    // アイテムウィンドウ
+    [SerializeField] GameObject _itemWindowObj = null;
+
+    // 表示用
+    [SerializeField] Button _showButton = null;
+
+    /// <summary>
+    /// 表示ボタンを押したときの処理
+    /// </summary>
+    public void OnShowButtonClick()
+    {
+        // アイテムウィンドウの表示・非表示
+        _itemWindowObj.SetActive(!_itemWindowObj.activeSelf);
+    }
+}
