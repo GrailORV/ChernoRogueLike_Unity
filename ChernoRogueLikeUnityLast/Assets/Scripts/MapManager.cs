459019daf0f188a87207209cb86f38ef42aa379d
SoraE1224 <sora-e1224@nifty.com>
2018-12-06 17:19:14 +0900

    MapManager & Player Movement

--- ChernoRogueLikeUnityLast/Assets/Scripts/MapManager.cs
+++ ChernoRogueLikeUnityLast/Assets/Scripts/MapManager.cs
@@ -1,38 +1,179 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    enum MapCategory
+    public enum EMapCategory
     {
-        MC_Grass,
-        MC_Cave
+        CAVE = 0,
+        MAX
+    }
+
+    public enum EFloorData
+    {
+        FLOOR = 0,
+        WALL,
+        WATER,
+        MAX
     }
 
     [SerializeField]
-    private MapCategory mCat;
+    private TextAsset _MapTextData;
 
-    [SerializeField]
-    private GameObject WallPrefab;
+    private string _MapName;
+    private EMapCategory _MapCategory;
+
+    [HideInInspector]
+    public uint _Width, _Height;
+
+    [HideInInspector]
+    public Texture[] _WallTextures = new Texture[(int)EMapCategory.MAX];
+    [HideInInspector]
+    public Texture[] _FloorTextures = new Texture[(int)EMapCategory.MAX];
+    [HideInInspector]
+    public Texture[] _WaterTextures = new Texture[(int)EMapCategory.MAX];
+
+    [HideInInspector]
+    public byte[,] _FloorMapData;
+    [HideInInspector]
+    public byte[,] _RoomMapData;
+    [HideInInspector]
+    public byte[,] _ObjMapData;
+
+    [HideInInspector]
+    public GameObject[,] _FloorObjects;
 
     void Start()
     {
-        //if(!WallPrefab)
-        //{
-        //    return;
-        //}
+        // Load Text Data As String
+        StringReader reader = new StringReader(_MapTextData.text);
+        string lineBuff;
 
-        //for (int x = 0; x < 100; x++)
-        //{
-        //    for (int z = 0; z < 300; z++)
-        //    {
-        //        Instantiate(WallPrefab, new Vector3(x, 0, z), Quaternion.identity);
-        //    }
-        //}
+        #region MapName
+
+        lineBuff = reader.ReadLine();
+        _MapName = lineBuff.Split(',')[1];
+
+        #endregion
+
+        #region Category
+
+        lineBuff = reader.ReadLine();
+        int category = int.Parse(lineBuff.Split(',')[1]);
+        _MapCategory = (EMapCategory)Enum.ToObject(typeof(EMapCategory), category);
+
+        #endregion
+
+        #region MapSize
+
+        lineBuff = reader.ReadLine();
+        _Width = uint.Parse(lineBuff.Split(',')[1]);
+        _Height = uint.Parse(lineBuff.Split(',')[3]);
+
+        _FloorMapData = new byte[_Width, _Height];
+        _RoomMapData = new byte[_Width, _Height];
+        _ObjMapData = new byte[_Width, _Height];
+        _FloorObjects = new GameObject[_Width, _Height];
+
+        #endregion
+
+        reader.ReadLine();
+        reader.ReadLine();
+
+        #region Import Map Floor Data & Create Map Floor
+
+        GameObject floorPrefab = Resources.Load("Prefabs/Floor") as GameObject;
+        GameObject wallPrefab = Resources.Load("Prefabs/Wall") as GameObject;
+        GameObject waterPrefab = Resources.Load("Prefabs/Water") as GameObject;
+
+        for (uint v = 0; v < _Height; v++)
+        {
+            lineBuff = reader.ReadLine();
+            var values = Array.ConvertAll(lineBuff.Split(','), int.Parse);
+            for (uint h = 0; h < _Width; h++)
+            {
+                Vector3 pos = new Vector3(h, 0, -v);
+                GameObject obj = new GameObject();
+                Material mat;
+                _FloorMapData[h, v] = (byte)values[h];
+                switch ((EFloorData)values[h])
+                {
+                    case EFloorData.FLOOR:
+                        obj = Instantiate(floorPrefab, pos, Quaternion.identity);
+                        mat = obj.GetComponentInChildren<Renderer>().material;
+                        mat.SetTexture("_MainTex", _FloorTextures[category]);
+                        obj.transform.parent = transform;
+                        break;
+                    case EFloorData.WALL:
+                        obj = Instantiate(wallPrefab, pos, Quaternion.identity);
+                        mat = obj.GetComponentInChildren<Renderer>().material;
+                        mat.SetTexture("_MainTex", _WallTextures[category]);
+                        obj.transform.parent = transform;
+                        break;
+                    case EFloorData.WATER:
+                        obj = Instantiate(waterPrefab, pos, Quaternion.identity);
+                        mat = obj.GetComponentInChildren<Renderer>().material;
+                        mat.SetTexture("_MainTex", _WaterTextures[category]);
+                        obj.transform.parent = transform;
+                        break;
+                    default:
+                        break;
+                }
+                _FloorObjects[h, v] = obj;
+            }
+        }
+
+        #endregion
+
+        reader.ReadLine();
+
+        #region RoomNum
+
+        lineBuff = reader.ReadLine();
+
+        #endregion
+
+        reader.ReadLine();
+
+        #region Import Room Data
+
+        for (uint v = 0; v < _Height; v++)
+        {
+            lineBuff = reader.ReadLine();
+            var values = Array.ConvertAll(lineBuff.Split(','), byte.Parse);
+            for (uint h = 0; h < _Width; h++)
+            {
+                _RoomMapData[h, v] = values[h];
+            }
+        }
+
+        #endregion
+
+        #region Import Map Object Data
+
+        for (uint v = 0; v < _Height; v++)
+        {
+            lineBuff = reader.ReadLine();
+            // var values = Array.ConvertAll(lineBuff.Split(','), byte.Parse);
+            for (uint h = 0; h < _Width; h++)
+            {
+                _ObjMapData[h, v] = 0;
+            }
+        }
+
+        #endregion
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
 
+        GUI.Label(new Rect(20, 20, 100, 20), _MapName);
+        GUI.Label(new Rect(20, 40, 100, 20), _MapCategory.ToString());
+        GUI.Label(new Rect(20, 60, 100, 20), "Width : " + _Width.ToString());
+        GUI.Label(new Rect(20, 80, 100, 20), "Height : " + _Height.ToString());
+        GUI.Label(new Rect(20, 100, 100, 20), _WallTextures.Length.ToString());
+
     }
+
 }
---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    MapPosition _pos = new MapPosition();
+    Transform _bodyTrans;
+
+    [SerializeField, Range(float.Epsilon, 10f)]
+    float _MoveDuration = 0.1f;
+
+    bool _EnableMove = false;
+
+    [HideInInspector]
+    public MapManager _MapManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        _MapManager = FindObjectOfType<MapManager>();
+        _EnableMove = true;
+        _bodyTrans = transform.Find("Body");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        if (!_EnableMove)
+        {
+            return;
+        }
+
+        if (_MapManager == null)
+        {
+            return;
+        }
+
+        int h = (int)Input.GetAxisRaw("Horizontal");
+        int v = (int)Input.GetAxisRaw("Vertical");
+
+        MapPosition newPos;
+
+        if (!Judge(h, v, out newPos))
+        {
+            return;
+        }
+
+        SetPosition(newPos, false);
+
+    }
+
+    private bool Judge(int diffX, int diffY, out MapPosition nextPos)
+    {
+        // Initialize Next Position
+        nextPos.x = 0;
+        nextPos.y = 0;
+
+        // Movement Value Check
+        if (diffX == 0 && diffY == 0)
+        {
+            return false;
+        }
+
+        // Look At Input Direction
+        Vector3 currentPos = _MapManager._FloorObjects[_pos.x, _pos.y].transform.position;
+        _bodyTrans.DOLookAt(new Vector3(currentPos.x + diffX, _bodyTrans.position.y, currentPos.z + diffY), _MoveDuration / 3f);
+
+        // Map Size Check
+        int xBuff = _pos.x + diffX;
+        if (xBuff < 0)
+        {
+            return false;
+        }
+        if (xBuff >= _MapManager._Width)
+        {
+            return false;
+        }
+
+        int yBuff = _pos.y - diffY;
+        if (yBuff < 0)
+        {
+            return false;
+        }
+        if (yBuff >= _MapManager._Height)
+        {
+            return false;
+        }
+
+        // Movement Root Check
+        if (_MapManager._FloorMapData[xBuff, _pos.y] == (byte)MapManager.EFloorData.WALL)
+        {
+            return false;
+        }
+
+        if (_MapManager._FloorMapData[_pos.x, yBuff] == (byte)MapManager.EFloorData.WALL)
+        {
+            return false;
+        }
+
+        // Goal Position Check
+        if (_MapManager._FloorMapData[xBuff, yBuff] != (byte)MapManager.EFloorData.FLOOR)
+        {
+            return false;
+        }
+
+        // return Suceeded
+        nextPos.x = (byte)xBuff;
+        nextPos.y = (byte)yBuff;
+
+        return true;
+    }
+
+    public void SetPosition(MapPosition pos, bool warp)
+    {
+        _pos = pos;
+
+        if (warp)
+        {
+            transform.position = _MapManager._FloorObjects[_pos.x, _pos.y].transform.position;
+            _bodyTrans.rotation = Quaternion.LookRotation(Vector3.back);
+        }
+        else
+        {
+            _EnableMove = false;
+            Vector3 newPos = _MapManager._FloorObjects[_pos.x, _pos.y].transform.position;
+
+            Sequence seq = DOTween.Sequence();
+            seq.Append(transform.DOMove(newPos, _MoveDuration))
+                .OnComplete(() =>
+                {
+                    _EnableMove = true;
+                });
+        }
+    }
+
+    public void SetPosition(byte x, byte y, bool warp)
+    {
+        SetPosition(new MapPosition(x, y), warp);
+    }
+}
