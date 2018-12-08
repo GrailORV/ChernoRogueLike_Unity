f7a0d0c8a488c15d44346c571b214601fc4d73a5
ueno <saku39sky@gmail.com>
2018-12-09 01:29:31 +0900

    シングルトンクラス(仮)の追加

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Manager/SingletonMonoBehaviour.cs
@@ -0,0 +1,28 @@
+﻿﻿using UnityEngine;
+ 
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {                
+                instance = (T)FindObjectOfType(typeof(T));
+ 
+                if (instance == null)
+                {
+                    Debug.LogError(typeof(T) + "がないからつくるで");
+ 
+                    var obj = new GameObject(typeof(T).ToString());
+                    obj.AddComponent(typeof(T));
+ 
+                    DontDestroyOnLoad(obj);
+                }
+            }
+ 
+            return instance;
+        }
+    }
+}
