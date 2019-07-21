731bbb1ab74c87f6732b3d40fbdff52a06a98396
ueno <saku39sky@gmail.com>
2019-07-21 22:51:24 +0900

    ウィンドウ用のアニメーションを作成

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Utility/TweenAnimation/AnimationType/ITweenAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace WindowAnimation
+{
+    public interface ITweenAnimation
+    {
+        bool  IsAnimation   { get; }
+        float AnimationTime { set; get; }
+        float DelayTime     { set; get; }
+
+        void SetUp(RectTransform rectT, float time, float delay);
+        void Open (Action onComplete);
+        void Close(Action onComplete); 
+    }
+}
