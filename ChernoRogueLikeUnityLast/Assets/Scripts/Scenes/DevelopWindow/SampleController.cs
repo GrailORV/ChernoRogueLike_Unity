340775ce82232bbe44fcf8c4b89974475bd74fdd
ueno <saku39sky@gmail.com>
2019-07-21 23:08:45 +0900

    サンプル用のクラスを作成

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Scenes/DevelopWindow/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleController : MonoBehaviour
+{
+    [SerializeField] SampleWindow _sampleWindow;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            _sampleWindow.Open();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _sampleWindow.Close();
+        }
+    }
+}
---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Scenes/DevelopWindow/SampleWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleWindow : WindowBase
+{
+    new void Awake()
+    {
+        base.Awake();
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public override void Open()
+    {
+        base.Open();
+    }
+}
