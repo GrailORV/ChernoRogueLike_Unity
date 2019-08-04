60f6732a174f249626e5a3aa549fa27e5549784e
ueno <saku39sky@gmail.com>
2019-08-04 22:21:10 +0900

    サンプルのプレハブを作成・ウィンドウクラスのBaseクラスを作成・マネージャークラスの仮作成

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Manager/Window/WindowManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// WindowBaseを管理するクラス。
+/// WindowBaseを継承しているプレハブの作成や削除・インスタンスの取得などを
+/// このクラスで行えるようにする。これで、参照させる負担やまとめて処理などを
+/// 行えるようにする（すべてのウィンドウを閉じるなど→実装するかはわからんが…）
+/// </summary>
+public class WindowManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
