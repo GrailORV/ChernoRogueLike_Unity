797a3ece77c8dc776cf74435836427a0c59f3d2e
ueno <saku39sky@gmail.com>
2019-09-14 22:56:48 +0900

    アイテムマネージャーの処理の作成

--- ChernoRogueLikeUnityLast/Assets/Scripts/Manager/Item/ItemManager.cs
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Manager/Item/ItemManager.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 public class ItemManager : SingletonMonoBehaviour<ItemManager>
 {
     // 表示するアイテムの最大数
-    public const int MAX_ITEM = 10;
+    public static readonly int MAX_SHOW_ITEM = 10;
 
     // アイテムのページの最大数
-    public const int MAX_PAGE = 3;
+    public static readonly int MAX_PAGE = 3;
 
-    // アイテムコントローラーのプレハブのパス
-    const string ITEM_PREFAB_PATH = "Prefabs/Scene/Item/ItemCanvas";
+    // アイテムの最大数
+    public static readonly int MAX_ITEM = MAX_SHOW_ITEM * MAX_PAGE;
 
-    // アイテムのコントローラークラス
-    ItemUIControl _itemControl = null;
+    // アイテムのマスタデータ
+    // key:アイテムのID value:アイテムデータ
+    Dictionary<int, ItemData> _mstItemData = new Dictionary<int, ItemData>();
 
-
+    // 全キャラクターのアイテム情報
+    // key:CharaID value:ItemData(List)
+    Dictionary<int, List<ItemData>> _itemDataDict = new Dictionary<int, List<ItemData>>();
 
     /// <summary>
     /// Instance生成時に呼ばれる処理
@@ -23,19 +28,205 @@ public class ItemManager : SingletonMonoBehaviour<ItemManager>
     protected override void Awake()
     {
         base.Awake();
-        Debug.Log("ItemManager OnAwake");
 
-        // アイテムの操作に必要なオブジェクトを生成
-        var prefab = Resources.Load(ITEM_PREFAB_PATH) as GameObject;
-        var itemCanvas = Instantiate(prefab, transform) as GameObject;
-        itemCanvas.name = prefab.name;
-        _itemControl = itemCanvas.GetComponent<ItemUIControl>();
+        // アイテムのマスターデータの取得
+        _mstItemData = ItemDataModifier.GetMstItemData();
 
-        // 表示
-        gameObject.SetActive(true);
-        itemCanvas.SetActive(true);
+        // 全キャラクターのアイテム情報を取得
+        // TODO ID管理と仮定しているので変更の可能性あり…現状はプレイヤーのみ
+        var dataList = ItemDataModifier.ReadItemData();
+        _itemDataDict.Add(0, dataList);
+    }
 
-        // アイテムクラスの初期化
-        _itemControl.Init();
+    /// <summary>
+    /// アイテム情報の取得
+    /// </summary>
+    /// <param name="charaId"></param>
+    /// <returns></returns>
+    public List<ItemData> GetItemData(int charaId)
+    {
+        // 指定したキャラIDのアイテムリストが存在するかどうか
+        if(!_itemDataDict.ContainsKey(charaId))
+        {
+            Debug.LogError("<color=red>charaIDが[" + charaId + "]のアイテムリストが存在しません</color>");
+            return null;
+        }
+
+        return _itemDataDict[charaId];
+    }
+
+    /// <summary>
+    /// アイテムの追加
+    /// </summary>
+    /// <param name="charaId">キャラID</param>
+    /// <param name="data">アイテムID</param>
+    /// <returns>結果</returns>
+    public bool AddItem(int charaId, int itemId)
+    {
+        // マスタデータが存在するか
+        if (_mstItemData == null)
+        {
+            Debug.LogError("<color=red>アイテムのマスタデータがないので追加できません</color>");
+            return false;
+        }
+
+        // idが存在するか確認
+        if (!_mstItemData.ContainsKey(itemId))
+        {
+            Debug.LogError("<color=red>ID = " + itemId + "のデータはマスタデータに存在しないです</color>");
+            return false;
+        }
+
+        // マスタデータからアイテムの情報を取得する
+        var data = _mstItemData[itemId];
+        return AddItem(charaId, data);
+    }
+
+    /// <summary>
+    /// アイテムの追加
+    /// </summary>
+    /// <param name="charaId">キャラID</param>
+    /// <param name="data">アイテムの情報</param>
+    /// <returns>結果</returns>
+    public bool AddItem(int charaId, ItemData data)
+    {
+        bool result = false;
+
+        // キャラクターが存在するか
+        if (!_itemDataDict.ContainsKey(charaId))
+        {
+            Debug.LogError("<color=red>charaIDが[" + charaId + "]のアイテムリストが存在しません</color>");
+            return result;
+        }
+
+        // データが存在するか
+        if (data == null)
+        {
+            Debug.LogError("<color=red>追加したいデータが存在しません</color>");
+            return result;
+        }
+
+        // 該当するキャラにアイテムを追加
+        var dataList = _itemDataDict[charaId];
+        for (int i = 0; i < MAX_ITEM; i++)
+        {
+            // 空きがあるか確認
+            if(0 < dataList[i].Id)
+            {
+                continue;
+            }
+
+            // アイテム追加
+            dataList[i] = data;
+            result = true;
+            break;
+        }
+
+        // UIの更新
+        return result;
+    }
+
+    /// <summary>
+    /// アイテムの削除
+    /// </summary>
+    /// <param name="charaId">キャラID</param>
+    /// <param name="itemId">アイテムID</param>
+    /// <returns></returns>
+    public bool DeleteItem(int charaId, int itemId)
+    {
+        // マスタデータが存在するか
+        if (_mstItemData == null)
+        {
+            Debug.LogError("<color=red>アイテムのマスタデータがないので追加できません</color>");
+            return false;
+        }
+
+        // idが存在するか確認
+        if (!_mstItemData.ContainsKey(itemId))
+        {
+            Debug.LogError("<color=red>ID = " + itemId + "のデータはマスタデータに存在しないです</color>");
+            return false;
+        }
+
+        // マスタデータからアイテムの情報を取得する
+        var data = _mstItemData[itemId];
+        return DeleteItem(charaId, data);
+    }
+
+    /// <summary>
+    /// アイテムの削除
+    /// </summary>
+    /// <param name="charaId">キャラID</param>
+    /// <param name="data">アイテムの情報</param>
+    /// <returns>結果</returns>
+    public bool DeleteItem(int charaId, ItemData data)
+    {
+        bool result = false;
+
+        // キャラクターが存在するか
+        if (!_itemDataDict.ContainsKey(charaId))
+        {
+            Debug.LogError("<color=red>charaIDが[" + charaId + "]のアイテムリストが存在しません</color>");
+            return result;
+        }
+
+        // データが存在するか
+        if (data == null)
+        {
+            Debug.LogError("<color=red>削除したいデータが存在しません</color>");
+            return result;
+        }
+
+        // 該当するキャラのアイテムを削除
+        var dataList = _itemDataDict[charaId];
+        result = dataList.Remove(data);
+
+        if(!result)
+        {
+            Debug.LogError("<color=red>削除したいデータを所持していません</color>");
+            return result;
+        }
+
+        // 空のデータを追加
+        dataList.Add(new ItemData());
+        return result;
+    }
+
+    /// <summary>
+    /// プレイヤーのアイテム追加
+    /// </summary>
+    /// <returns></returns>
+    public bool AddPlayerItem(int itemId)
+    {
+        return AddItem(0, itemId);
+    }
+
+    /// <summary>
+    /// プレイヤーのアイテム追加
+    /// </summary>
+    /// <returns></returns>
+    public bool AddPlayerItem(ItemData data)
+    {
+        return AddItem(0, data);
+    }
+
+    /// <summary>
+    /// プレイヤーのアイテム削除
+    /// </summary>
+    /// <param name="itemId"></param>
+    /// <returns></returns>
+    public bool DeletePlayerItem(int itemId)
+    {
+        return DeleteItem(0, itemId);
+    }
+
+    /// <summary>
+    /// プレーヤーのアイテム削除
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public bool DeletePlayerItem(ItemData data)
+    {
+        return DeleteItem(0, data);
     }
 }
