f5a8722757cfc6af8c0ffb75d7539cb570487e89
ueno <saku39sky@gmail.com>
2020-05-30 22:51:49 +0900

    テスト用テーブルデータの用意

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/ScriptableObjectGenerator/DataCS/ItemTableData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTableData : ScriptableObject
+{
+    /// <summary> データリスト </summary>
+    public List<Data> dataList = new List<Data>();
+
+    [System.Serializable]
+    public class Data
+    {
+        public int id;
+        public string type;
+        public string name;
+    }
+}
