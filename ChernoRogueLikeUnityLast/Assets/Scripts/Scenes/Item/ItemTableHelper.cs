0c3add74d7a5cd450b71bf5cea2f48bc79e0e71d
ueno <saku39sky@gmail.com>
2020-06-14 00:34:17 +0900

    アイテムのマスタテーブルからデータを取得するクラスを作成

--- ChernoRogueLikeUnityLast/Assets/Scripts/Scenes/Item/ItemTableHelper.cs
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Scenes/Item/ItemTableHelper.cs
@@ -1,19 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class ItemTableHelper
 {
     /// <summary>
     /// アイテムのマスターデータ
+    /// key:id value:ItemTableData.Data
     /// </summary>
-    public static List<ItemData> MstItemData { get; private set; }
+    public static Dictionary<int, ItemTableData.Data> MstItemData { get; private set; }
 
     /// <summary>
     /// テーブルの読み込み
     /// </summary>
     public static void Load()
     {
-        //MstItemData = Resources.Load<ItemTabe>("MasterData/ItemTable").
+        var dataList = Resources.Load<ItemTableData>("MasterData/ItemTable").dataList;
+
+        // リストディクショナリに変換する
+        MstItemData = dataList.ToDictionary(data => data.id);
+    }
+
+    /// <summary>
+    /// アイテムがあるかどうか(ID 基準)
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public static bool ContainsItemId(int id)
+    {
+        return MstItemData.ContainsKey(id);
+    }
+
+    /// <summary>
+    /// マスタデータからアイテムを取得
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public static ItemTableData.Data GetItemData(int id)
+    {
+        if(MstItemData.ContainsKey(id))
+        {
+            return MstItemData[id];
+        }
+        else
+        {
+            return null;
+        }
     }
 }
