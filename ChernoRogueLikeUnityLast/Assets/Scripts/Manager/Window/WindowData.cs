0d815becc66d3171203e93cde5a1230f820ca3ba
ueno <saku39sky@gmail.com>
2020-08-01 23:56:22 +0900

    ツボ用のアイテムウィンドウを追加

--- ChernoRogueLikeUnityLast/Assets/Scripts/Manager/Window/WindowData.cs
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Manager/Window/WindowData.cs
@@ -7,13 +7,17 @@ public static class WindowData
 {
 	public enum WindowType
 	{
+		InventoryWindow,
 		ItemWindow,
+		PotItemWindow,
 		SampleWindowPanel,
 	}
 
 	public readonly static Dictionary<WindowType, string> WindowPathDict = new Dictionary<WindowType, string>()
 	{
+		{ WindowType.InventoryWindow , "Prefabs/Scene/Item/InventoryWindow" },
 		{ WindowType.ItemWindow , "Prefabs/Scene/Item/ItemWindow" },
+		{ WindowType.PotItemWindow , "Prefabs/Scene/Item/PotItemWindow" },
 		{ WindowType.SampleWindowPanel , "Prefabs/Scene/SampleWindowManager/SampleWindow(Panel)" },
 	};
 }
---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Scenes/Item/PotItemWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotItemWindow : ItemWindowBase
+{
+    public override void SetUp()
+    {
+        // ページャーの初期化
+        _pager.Init(MaxPage);
+
+        // 初期化
+        CurrentPageIndex = 0;
+
+        // アイテム情報の更新
+        SetData(GetDataList());
+
+        ItemTableHelper.Load();
+        AddItem(new List<int>() { 1, 1, 1});
+        
+        // アイテム一覧ないなら作成
+        if (_itemCellList == null || _itemCellList.Count != SHOW_ITEM_NUM)
+        {
+            CreateItemCell();
+        }
+        // 作成済みならUIの更新
+        else
+        {
+            UpdateCurrentPageItemUI();
+        }
+    }
+}
