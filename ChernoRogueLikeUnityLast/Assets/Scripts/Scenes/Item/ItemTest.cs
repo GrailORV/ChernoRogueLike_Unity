75883dabaf6df9d0a4084dab3de8a1691dd42f2f
ueno <saku39sky@gmail.com>
2020-11-21 22:42:36 +0900

    確認用のコードを残しておく

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Scenes/Item/ItemTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTest : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        var window = WindowManager.Instance.CreateAndOpenWindow<ItemWindow>(WindowData.WindowType.ItemWindow);
+        window.SetUp();
+        return;
+	}
+}
