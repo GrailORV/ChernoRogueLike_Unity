4b1e5af5bb3d3e7dcd1538531d15cbb90635fc72
DESKTOP-6OKMHTG\SoraE1224 <sora-e1224@nifty.com>
2021-01-16 23:24:48 +0900

    ダンジョンの仕組み一新のためソースコードの整理

---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Dungeon/DungeonManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Dungeon
+{
+	/// <summary>
+	/// ダンジョンマネージャー
+	/// </summary>
+	public class DungeonManager : SingletonMonoBehaviour<DungeonManager>
+	{
+		
+	}
+}
---
+++ ChernoRogueLikeUnityLast/Assets/Scripts/Dungeon/MapManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Dungeon
+{
+	/// <summary>
+	/// マップ生成マネージャー
+	/// </summary>
+	public class MapManager : SingletonMonoBehaviour<MapManager>
+	{
+
+	}
+}
